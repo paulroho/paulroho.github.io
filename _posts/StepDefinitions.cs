476433240f37476a1e807be621067dea50e8b67a
Paul Rohorzka <paul.rohorzka@techtalk.at>
2014-06-04 06:03:35 +0200

    SpecFlow-Post: reproduction assets

---
+++ _posts/StepDefinitions.cs
@@ -0,0 +1,14 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SpecFlowPlayground
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        [Given(@"a user '(.*)' is logged in")]
+        public void GivenAUserIsLoggedIn(string username)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
